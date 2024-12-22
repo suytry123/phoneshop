1c68745fd0722d7d463246fe6e024320de4ded42
Suytry Sok <soksoytry2000@gmail.com>
2024-12-22 22:57:18 +0700

    jwt

--- src/main/java/com/java/school/phoneshop/config/security/SecurityConfig.java
+++ src/main/java/com/java/school/phoneshop/config/security/SecurityConfig.java
@@ -6,27 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.java.school.phoneshop.config.jwt.JwtLoginFilter;
+import com.java.school.phoneshop.config.jwt.TokenVerifyFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(
 		  prePostEnabled = true, 
 		  securedEnabled = true, 
 		  jsr250Enabled = true)
-@EnableWebMvc
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
+			.addFilter(new JwtLoginFilter(authenticationManager()))
+			.addFilterAfter(new TokenVerifyFilter(), JwtLoginFilter.class)
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
 			.authorizeHttpRequests()
 			.antMatchers("/","index.html","css/**","js/**").permitAll()
 			//.antMatchers("/models").hasRole(RoleEnum.SALE.name()) // "SALE"
@@ -34,33 +40,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			//.antMatchers(HttpMethod.POST, "/brand").hasAuthority(BRAND_WRITE.getDescription())
 			//.antMatchers(HttpMethod.GET, "/brand").hasAuthority(BRAND_READ.getDescription())
 			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic();
+			.authenticated();
 	}
 
 	@Bean
 	@Override
 	protected UserDetailsService userDetailsService() {
-
 		//User user1 = new User("dara", passwordEncoder.encode("dara123"), Collections.emptyList());
-
+		
 		UserDetails user1 = User.builder()
 				.username("dara")
 				.password(passwordEncoder.encode("dara123"))
-				//.roles("SALE") //ROLE_SALE
-				.authorities(RoleEnum.SALE.getAuthorities()) //collection of GrantedAuthority
+				//.roles(RoleEnum.SALE.name()) //ROLE_SALE
+				.authorities(RoleEnum.SALE.getAuthorities()) // collection of GrantedAuthority
 				.build();
 		//GrantedAuthority
-
+		
 		UserDetails user2 = User.builder()
 				.username("thida")
 				.password(passwordEncoder.encode("thida123"))
 				.authorities(RoleEnum.ADMIN.getAuthorities()) // ROLE_ADMIN
 				.build();
-
+		
 		UserDetailsService userDetailsService = new InMemoryUserDetailsManager(user1, user2);
-
+		
 		return userDetailsService;
 	}
 }
